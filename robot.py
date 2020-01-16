726180e41fe55febafcdcbac1e561cd9385ae0c0
Nathan Beasley <tuffinmuffin@gmail.com>
2020-01-15 20:03:43 -0700

    created simple 2020 robot example with TImedRobot. Since command based robot not ready

--- robot.py
+++ robot.py
@@ -6,19 +6,5 @@ import wpilib
 from team3200 import Robot as Robot
 
 if __name__ == '__main__':
-    try:
-        # patch no exit error if not running on robot
-        try:
-            print(wpilib._impl.main.exit)
-        except Exception:
-            wpilib._impl.main.exit = exit
-
-        # fixes simulation rerun errors.
-        # todo verify this causes no issues on robot
-        wpilib.DriverStation._reset()
-
-    except Exception as err:
-        print("Failed to do extra setup. Error", err)
-
     wpilib.run(Robot, physics_enabled=True)
 
--- team3200/robot.py
+++ team3200/robot.py
@@ -1,9 +1,17 @@
 #import team3200
-import commandbased
+#import commandbased
 
+from wpilib import TimedRobot
+from wpilib import Talon
 
-class Robot(commandbased.CommandBasedRobot):
+class Robot(TimedRobot):
 
     def robotInit(self):
         '''This is where the robot code starts.'''
-        pass
+        self.talon = Talon(1)
+
+    def autonomousPeriodic(self):
+        self.talon.set(.5)
+    
+    def teleopInit(self):
+        self.talon.set(0)
